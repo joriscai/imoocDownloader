8f3a11d50f885d89ca42c80f8e5603ebb1ea85d7
Krew <wanhao.huang@outlook.com>
2016-08-26 17:06:23 +0800

    添加dom解析／获取

--- src/htmlPaser.js
+++ src/htmlPaser.js
@@ -1,18 +1,30 @@
 'use strict';
 
+let vm = require('vm');
 let cheerio = require('cheerio');
+let rule = require('../conf/rule');
+let ruleParser = require('../lib/ruleParser');
 
 
 function HtmlParser(html) {
-  this.html = html;
+  vm.createContext(this);
+  this.$ = this.dom = cheerio.load(html);
 }
 
 
-HtmlParser.prototype.parseRule = function(rule) {
-  
-}
-
-
-HtmlParser.prototype.dom = function(html) {
-  this.dom = cheerio.load(html);
+HtmlParser.prototype.getter = function(opts,callback) {
+  this.opts = opts;
+  this.callback = callback;
+  let query = ruleParser(opts.rule);
+  vm.runInContext(
+    `var list = ${query};
+    list.each(function(index,ele) {
+      if(opts.prop){
+        callback(null,$(ele).attr(opts.propValue));
+      }else{
+        callback(null,$(ele).text());
+      }
+    });`, 
+    this
+  );
 }
